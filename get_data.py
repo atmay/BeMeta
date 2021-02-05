20edea30007256a1aaa17b13d477918f4d48795a
atmay <liubov.burtseva@gmail.com>
2021-02-05 11:37:48 +0300

    create and delete data operations added

--- get_data.py
+++ get_data.py
@@ -2,89 +2,108 @@ import psycopg2
 from datetime import date
 import requests
 
-# СКРИПТ
-#
-# + Получить из Airtable все записи
-# + Получить из таблицы постгре все записи
-# - Проверить, какие записи из Airtable не существуют в Постгрес
-#   - добавить записи в постгрес
-# - Если в Airtable нет записей, которые есть в Постгрес:
-#   - удалить из Постгрес
-# - загрузить raw data в таблицу
+"""
+Получаем raw data из Airtable и превращаем их в словарь с ключом id записи в Airtable
+"""
 
-
-# выгружаем сырые данные из Airtable
 AIRTABLE_BASE_ID = 'appPFtPl42cKzPk1f'
 AIRTABLE_TABLE_NAME = 'Psychotherapists'
 API_KEY = 'keyTB6bXM5hmHID2v'
-
 ENDPOINT = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
 
 headers = {'Authorization': f'Bearer {API_KEY}'}
 raw_data = requests.get(url=ENDPOINT, headers=headers).json()
 
-# распарсиваем данные из json в словарь с ключом - id записи в Airtable
-cleaned_data = {}
+airtable_cleaned_data = {}
 
 for i in range(len(raw_data['records'])):
     id = raw_data['records'][i]['id']
-    tmp = []
-    tmp.append(raw_data['records'][i]['fields']['Имя'])
-    tmp.append(raw_data['records'][i]['fields']['Методы'])
-    tmp.append(raw_data['records'][i]['fields']['Фотография'][0]['id'])
-    tmp.append(raw_data['records'][i]['fields']['Фотография'][0]['url'])
-    tmp.append(raw_data['records'][i]['createdTime'])
-    cleaned_data[id] = tmp
+    tmp = [raw_data['records'][i]['fields']['Имя'],
+           raw_data['records'][i]['fields']['Методы'],
+           raw_data['records'][i]['fields']['Фотография'][0]['id'],
+           raw_data['records'][i]['fields']['Фотография'][0]['url'],
+           raw_data['records'][i]['createdTime']]
+    airtable_cleaned_data[id] = tmp
 
-print(cleaned_data)
+print(airtable_cleaned_data)
+
+""" 
+Устанавливаем соединение с PostgreSQL
+"""
+conn = psycopg2.connect("dbname=meta_db user=postgres password=MetaMeta1")
+cur = conn.cursor()
 
 """
-Нужно обработать 4 случая:
+Заливаем raw data в соответствующую таблицу
+"""
+# raw_data_to_add = str(raw_data)
+# date_today = date.today()
+# cur.execute(
+#     "INSERT INTO therapists_rawdata (date, raw_data) "
+#     "VALUES (%s, %s)", (date_today, raw_data_to_add))
+
+# # опционально - можно сразу в консоли проверить успешность выгрузки
+# cur.execute("SELECT * FROM therapists_rawdata;")
+# r = cur.fetchall()
+# print(r)
+
+"""
+Обновляем основную таблицу. Нужно обработать 4 случая:
     - запись есть в Airtable, но отсутствует в Postgres -> добавить запись
     - записи нет в Airtable, но она есть в Postgres -> удалить запись
     - запись есть в обеих базах, но в Airtable изменились данные -> обновить данные
     - запись есть в обеих базах и изменений нет -> skip
 """
 
-# подключаемся к базе данных PostgreSQL
-conn = psycopg2.connect("dbname=meta_db user=postgres password=MetaMeta1")
-cur = conn.cursor()
-
-
-
-# cur.execute("INSERT INTO therapists_therapist(therapist_id, name, methods, photo_id, photo_link, created_time) "
-#             "VALUES(%s, %s, %s, %s, %s, %s)", ('rec9dKijJaU9vWtoK',
-#                                                'Василий',
-#                                                ['Психоанализ', 'Коучинг', 'Музыкотерапия'],
-#                                                'attY7G4ysiIDnSaUw',
-#                                                'https://dl.airtable.com/.attachments/7da0d4c7963babf742137abc4e9a1a99/5f547505/1.jpg',
-#                                                '2021-02-02T14:29:36.000Z'))
-
-# для проверки наличия/ отсутствия записей используем therapist_id
+"""
+Для оптимизации удаления и добавления данных получаем только значения therapist_id из Postgres
+"""
 cur.execute("SELECT therapist_id FROM therapists_therapist ")
-ids_from_airtable = cur.fetchall()
+ids_from_postgres = cur.fetchall()
+ids_from_postgres = [i[0] for i in ids_from_postgres]
 
-# пробегаем по ключам в Postgres и удаляем те записи, которых нет в Airtable
-for i in ids_from_airtable:
-    if i[0] not in cleaned_data.keys():
-        id_to_delete = i[0]
-        cur.execute("DELETE FROM therapists_therapist WHERE therapist_id = (%s,)", (id_to_delete))
+"""
+По полю therapist_id проверяем наличие в Postgres записей, которые были удалены из Airtable, удаляем их 
+"""
+for i in ids_from_postgres:
+    if i not in airtable_cleaned_data.keys():
+        cur.execute("DELETE FROM therapists_therapist WHERE therapist_id = %s", (i,))
 
+"""
+По полю therapist_id проверяем наличие в Airtable  записей, которые отсутствуют в Postgres, добавляем их
+"""
+for i in airtable_cleaned_data.keys():
+    if i not in ids_from_postgres:
+        ther_id = i
+        ther_name = airtable_cleaned_data[i][0]
+        ther_methods = airtable_cleaned_data[i][1]
+        ther_photo_id = airtable_cleaned_data[i][2]
+        ther_photo_link = airtable_cleaned_data[i][3]
+        ther_created_time = airtable_cleaned_data[i][4]
+        cur.execute("INSERT INTO therapists_therapist(therapist_id, name, methods, photo_id, photo_link, created_time) "
+                    "VALUES(%s, %s, %s, %s, %s, %s)", (
+                        ther_id, ther_name, ther_methods, ther_photo_id, ther_photo_link, ther_created_time
+                    ))
+
+"""
+Проверяем на актуальность существующие записи в Postgres, при необходимости обновляем их
+"""
+for i in ids_from_postgres:
+    cur.execute("SELECT therapist_id, name, methods, photo_id, photo_link, created_time FROM therapists_therapist "
+                "WHERE therapist_id = %s", (i,))
+    postgres_row = cur.fetchall()
+    ther_id = i
+    ther_name = postgres_row[0][1]
+    ther_methods = postgres_row[0][2]
+    ther_photo_id = postgres_row[0][3]
+    ther_photo_link = postgres_row[0][4]
+    ther_created_time = postgres_row[0][5]
+    print(postgres_row)
+# [('recuJ6e1pvG6tkRdC', 'Иннокентий', '{Гештальт-терапия,Коучинг,Психосинтез,Сказкотерапия}',
+# 'atttxEgUknSdwDjme', 'https://dl.airtable.com/.attachments/fa70928a82a214d22c4b7a2eeace79d2/e5a12360/2.jpg',
+# '2021-02-02T14:29:36.000Z')]
 # postgres_table = cur.fetchall()
 # print(postgres_table)
-
-
-# добавляем сырые данные и дату выгрузки в таблицу raw_data
-# raw_data_to_add = str(raw_data)
-# date_today = date.today()
-# cur.execute(
-#     "INSERT INTO therapists_rawdata (date, raw_data) "
-#     "VALUES (%s, %s)", (date_today, raw_data_to_add))
-#
-# # можно сразу проверить успешность выгрузки в консоли
-# cur.execute("SELECT * FROM therapists_rawdata;")
-# r = cur.fetchall()
-# print(r)
 
 conn.commit()
 
